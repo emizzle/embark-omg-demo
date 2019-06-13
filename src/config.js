fcf0c2670df1745a417978bc1a9cf1e36a7c6a44
emizzle <eric.mastro@gmail.com>
2019-06-13 18:27:50 +1000

    Add `js-starter-kit` to DApp
    Get vue.js working with embark

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const localConfig = {
+  web3ProviderUrl: 'https://rinkeby.infura.io/',
+  watcherUrl: 'http://watcher.samrong.omg.network/',
+  childchainUrl: 'http://samrong.omg.network/',
+  plasmaContractAddress: '0x740ecec4c0ee99c285945de8b44e9f5bfb71eea7'
+}
+
+module.exports = localConfig
